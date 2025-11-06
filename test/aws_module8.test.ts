635cb6e551542b2e333051d123bd975f5aa33827
Ricardo Quinonez <vir_qup_91@hotmail.com>
2025-11-05 22:19:03 -0600

    Initial commit

---
+++ test/aws_module8.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsModule8 from '../lib/aws_module8-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_module8-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsModule8.AwsModule8Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
